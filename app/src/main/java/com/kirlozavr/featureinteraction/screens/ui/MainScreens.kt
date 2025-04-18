71256df723bc0d6605cb2d055ba812f6dbf87152
kirlozavr <kirill.protchenko184@gmail.com>
2025-04-18 11:08:15 +1000

    Добавил кнопку отмены и новые состояния

--- app/src/main/java/com/kirlozavr/featureinteraction/screens/ui/MainScreens.kt
+++ app/src/main/java/com/kirlozavr/featureinteraction/screens/ui/MainScreens.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -66,7 +67,8 @@ internal fun MessageScreen(
                 Text(
                     text = state.message,
                     modifier = Modifier.padding(16.dp),
-                    fontSize = 20.sp
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
                 )
             }
         },
